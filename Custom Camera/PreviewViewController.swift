86def980057d7c7ce2e1ec50142582073f93c4ef
Rudra Jikadra <rhjikadra@gmail.com>
2018-06-18 02:02:37 +0530

    Initial Commit

---
+++ Custom Camera/PreviewViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PreviewViewController.swift
+//  Custom Camera
+//
+//  Created by Rudra Jikadra on 08/12/17.
+//  Copyright © 2017 Rudra Jikadra. All rights reserved.
+//
+
+import UIKit
+
+
+class PreviewViewController: UIViewController {
+
+    
+    @IBOutlet weak var photo: UIImageView!
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photo.image = self.image
+        
+        // Do any additional setup after loading the view.
+      
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonTouch(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTouch(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
